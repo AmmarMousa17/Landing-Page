07d5c219d198451f3d2732cfb3e3df1d79ee5128
Ammar <ammar_mousa17@hotmail.com>
2020-07-15 17:00:22 +0200

    initial

---
+++ README.md
@@ -0,0 +1,17 @@
+# Landing Page
+This project is for Udacity's Nanodegree program
+
+##Description
+Multisection landing page dynamiclly add content with nav menu
+
+## the languages used:
+- HTML
+- CSS
+- JAVASCRIPT
+
+
+## Running project:
+-download project 
+-extract files
+-open your browser
+-open index.html on your browser
---
+++ js/app.js
@@ -0,0 +1,87 @@
+
+
+/**
+ * Define Global Variables
+ * 
+*/
+// navbar global var
+const navbar = document.getElementById('navbar__list');
+// sections global var
+const sections = document.querySelectorAll('section');
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+
+// build the nav
+
+const navBuilder = () => {
+
+    let  Uli = document.createElement('ul');
+    // looping over all sections
+    sections.forEach(section => {
+
+        const ID = section.getAttribute("id");
+        const DataNav = section.getAttribute("data-nav");
+
+
+        Uli += `<li><a class="menu__link" href="#${ID}">${DataNav}</a></li>`;
+
+    });
+    // append  list items of clicked sections to the navbar
+    navbar.innerHTML = Uli;
+
+
+};
+
+navBuilder();
+
+
+// Scroll to section on link click
+const ScrollToSectionOnLinkClick = () => {
+
+    const anchors = document.querySelectorAll('.navbar__menu a');
+    anchors.forEach(anchor => {
+        anchor.addEventListener('click', function(event) {
+             document.querySelector(anchor.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+
+        });
+    });
+
+};
+
+ScrollToSectionOnLinkClick();
+
+// adding / removing the active class  on view
+
+const sectionView = () => {
+    sections.forEach(section => {
+    	//claculating the nearing section when viewing
+        const elementOffset =  Math.floor(section.getBoundingClientRect().top);
+
+        inviewport = () => elementOffset < 150 && elementOffset >= -150;
+
+       //remove the active class
+        section.classList.remove('your-active-class');
+        var x=  section.querySelector('h2');
+        x.style.cssText="color:white";  
+        //Add the active class
+        if(inviewport()){
+        section.classList.add('your-active-class');
+        var x=  section.querySelector('h2');
+        x.style.cssText="color:green"; //To clearwhich section is being viewed
+         
+        }
+        
+    });
+};
+
+window.addEventListener('scroll' ,sectionView);
+
+
